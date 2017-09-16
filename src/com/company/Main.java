de1f63c35da79bba9a98f4accae0388b19edd627
klvchvncnt <vk785@meta.ua>
2017-09-16 23:36:57 +0300

    Test 11-36

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        boolean t = true;
+        first:
+        {
+            second:
+            {
+                third:
+                {
+                    System.out.println("Before the break.");
+                    if (t) break second;
+                    System.out.println("This wont execute");
+                }
+                System.out.println("This wont execute");
+            }
+            System.out.println("This is after second block.");
+        }
     }
 }
