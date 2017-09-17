2fa3825dd59b7b5c113b4cc181d2fb7839f33a1f
kwchvncnt <vk785@meta.ua>
2017-09-17 20:31:02 +0300

    Task 1 part1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String s1 = "Java";
-        String s2 = new String(s1);
-        System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
+        Scanner un = new Scanner(System.in);//user greetings
+        System.out.print("Hello! Please type tour name here:");
+        String user_name = un.next();
+
+        System.out.println("Hello " + user_name);
     }
+
 }
+
