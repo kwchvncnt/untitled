c86ed5ee3edb751b923a92ca3506605701efce58
kwchvncnt <vk785@meta.ua>
2017-09-23 19:58:51 +0300

    Task 1

---
+++ src/com/company/Task_1_2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+
+//revert command line output
+public class Task_1_2 {
+    public static void main(String[] args) {
+        for (String x : args) {
+            System.out.println("x = " + x);
+        }
+    }
+
+}
